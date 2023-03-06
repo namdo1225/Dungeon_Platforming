4733a0da1a43c169d6d155d46435a73937ccbed0
slimMVD <72676610+slimMVD@users.noreply.github.com>
2023-03-05 21:36:47 -0700

    Add files via upload

---
+++ Assets/Scripts/Alex/GameOverScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Game Scene");
+        SceneManager.LoadScene(4);
+    }
+}
