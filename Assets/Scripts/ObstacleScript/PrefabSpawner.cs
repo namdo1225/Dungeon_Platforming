7d6714b4eb18d112cf848224b7e213457cf223ed
namdo1225 <110580990+namdo1225@users.noreply.github.com>
2023-03-06 18:40:19 -0700

    20230306_MoreScriptComments

--- Assets/Scripts/ObstacleScript/PrefabSpawner.cs
+++ Assets/Scripts/ObstacleScript/PrefabSpawner.cs
@@ -1,3 +1,7 @@
+/**
+ * Description: Class to handle spawning of a prefab (usually enemies) when player is in a defined collision area.
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +11,9 @@ public class PrefabSpawner : MonoBehaviour
     public GameObject enemyPrefab;
     public List<GameObject> SpawnPoints;
 
+    // When the player entered the collision zone, spawn a defined prefab at the location of the children objects.
     void OnTriggerEnter(Collider col)
     {
-
         GameObject gameObject = col.gameObject;
         if (gameObject.tag == "Player"){
             foreach (GameObject point in SpawnPoints)
